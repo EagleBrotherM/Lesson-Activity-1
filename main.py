3b0e470ab1cf21d19931b06257a7493400538b0d
EagleBrotherM <mihir161211@gmail.com>
2024-12-26 14:24:50 +0545

    1

---
+++ main.py
@@ -0,0 +1,12 @@
+print ('How Many Players Are Allowed In Football')
+print('1. --> 11\n')
+print('2. --> 6\n')
+print('3. --> 14\n')
+print('4. --> 100')
+answer = input('enter your choice:')
+
+if answer == '1':
+    print('Intelligent') 
+else:
+    print('Try Hard Next Time')
+
